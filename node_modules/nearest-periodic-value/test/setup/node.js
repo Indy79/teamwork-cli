b4d18d701c17904b6a00cf469bbc352fbf559612
Jean-baptiste JOFFRE <jeanbaptiste.joffre@gmail.com>
2018-05-25 14:39:19 +0200

    remove node_modules

--- node_modules/nearest-periodic-value/test/setup/node.js
+++
@@ -1,8 +0,0 @@
-var setup = require('./setup');
-var config = require('../../config');
-
-global[config.exportVarName] = require('../../src/' + config.entryFileName);
-global.chai = require('chai');
-global.sinon = require('sinon');
-global.chai.use(require('sinon-chai'));
-setup();
